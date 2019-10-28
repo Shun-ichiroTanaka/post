35928a7dd4212988e9cc9f7a0c4120a7d7fdb123
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-28 16:32:17 +0900

    app.jsの位置変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,12 @@
 
     <title>{{ config('app.name', 'Step') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -28,5 +25,9 @@
         </main>
         @include('layouts.footer')
     </div>
+
+<!-- Scripts -->
+<script src="{{ mix('js/app.js') }}" defer></script>
+
 </body>
 </html>
