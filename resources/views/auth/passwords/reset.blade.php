36d1760d7fd743dd4e1fb7cc6881240eeca92034
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-28 10:07:04 +0900

    emailパスワードリセット画面完成

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -9,10 +9,8 @@
 
         <input type="hidden" name="token" value="{{ $token }}">
 
-        <div>
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
+        <div class="p-auth__form-reset">
+            <input id="email" placeholder="{{ __('E-Mail Address') }}" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -20,9 +18,8 @@
             @enderror
         </div>
 
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        <div class="p-auth__form-reset">
+            <input id="password" placeholder="{{ __('Password') }}" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -30,12 +27,13 @@
             @enderror
         </div>
 
-        <div>
-            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+        {{-- パスワード確認 --}}
+        <div class="p-auth__form-reset">
+           <input id="password-confirm" placeholder="{{ __('Password') }}" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
         </div>
 
-        <button type="submit" class="btn btn-primary">{{ __('Reset Password') }}</button>
+        {{-- 　送信ボタン　　 --}}
+        <button type="submit">{{ __('Reset Password') }}</button>
 
     </form>
 </div>
