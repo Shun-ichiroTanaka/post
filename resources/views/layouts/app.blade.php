5c96ce8fa8bfae989f9341e2071da397ca7280ac
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-11-03 23:34:25 +0900

    修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,6 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    {{-- markdownライブラリ --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplemde@latest/dist/simplemde.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/simplemde@latest/dist/simplemde.min.js"></script>
-
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
@@ -40,70 +36,7 @@
 
 
     @include('sweetalert::alert')
-    <script>
-        // const markdown = new SimpleMDE({
-        //     element: document.getElementById("markdown")
-        // });
-        // Most options demonstrate the non-default behavior
-        const simplemde = new SimpleMDE({
-            element: document.getElementById("markdown"),
-            autofocus: true,
-            autosave: {
-                enabled: true,
-                delay: 1000,
-            },
-            forceSync: true,
-            hideIcons: ["guide", "heading"],
-            indentWithTabs: true,
-            insertTexts: {
-                horizontalRule: ["", "\n\n-----\n\n"],
-                image: ["![](http://", ")"],
-                link: ["[", "](http://)"],
-                table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
-            },
-            lineWrapping: true,
-            parsingConfig: {
-                allowAtxHeaderWithoutSpace: true,
-                strikethrough: true,
-                underscoresBreakWords: true,
-            },
-            placeholder: "Markdown記法で書いてみよう",
-            previewRender: function(plainText) {
-                return customMarkdownParser(plainText); // Returns HTML from a custom parser
-            },
-            previewRender: function(plainText, preview) { // Async method
-                setTimeout(function(){
-                    preview.innerHTML = customMarkdownParser(plainText);
-                }, 250);
 
-                return "Loading...";
-            },
-            promptURLs: true,
-            renderingConfig: {
-                singleLineBreaks: true,
-                codeSyntaxHighlighting: true,
-            },
-            shortcuts: {
-                drawTable: "Cmd-Alt-T"
-            },
-            showIcons: ["code", "table"],
-            spellChecker: true,
-            status: true,
-            status: ["autosave", "lines", "words", "cursor"], // Optional usage
-            status: ["autosave", "lines", "words", "cursor", {
-                className: "keystrokes",
-                defaultValue: function(el) {
-                    this.keystrokes = 0;
-                    el.innerHTML = "0 Keystrokes";
-                },
-                onUpdate: function(el) {
-                    el.innerHTML = ++this.keystrokes + " Keystrokes";
-                }
-            }], // Another optional usage, with a custom status bar item that counts keystrokes
-            styleSelectedText: true,
-            tabSize: 4,
-        });
-    </script>
     {{-- vueを用いるためこの位置 --}}
     <script src=" {{ mix('js/app.js') }} "></script>
 </body>
--- resources/views/posts/new.blade.php
+++ resources/views/posts/new.blade.php
@@ -1,13 +1,84 @@
-{{--
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    新規投稿画面
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script>window.Laravel = { csrf-token: '{{ csrf_token() }}' }</script>
 
+    <title>{{ config('app.name', 'Step') }}</title>
 
-    --}}
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    @extends('layouts.app')
+    {{-- markdownライブラリ --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplemde@latest/dist/simplemde.min.css">
 
-    @section('content')
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+<body>
+<header class="l-header">
+		<h1 class="l-header__title"><a href="{{ url('/') }}"> {{ config('app.name', 'Step') }}</a></h1>
+		<nav class="l-header__nav">
+			<ul>
+                @guest
+                <li>
+                    <a class="l-header__signin" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                <li>
+                @if (Route::has('register'))
+                    <li>
+                        <a class="l-header__signup" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+                @else
+                    <li>
+                        <a href="{{ route('post.new') }}">Post</a>
+                        {{-- <a href="{{ route('post.index') }}"></a> --}}
+                    </li>
+                    <li class="l-header-dropdown">
+                        <ul>
+                            <li class="l-header-dropdown__child">
+                                <div href="#" class="l-header-dropdown__child-user">
+                                    {{-- {{ Auth::user()->name }} --}}
+                                    @if (empty(Auth::user()->avatar))
+                                        <img src="/img/avatar/user.svg" alt="avatar" style="width:40px; height:40px; border-radius:50%" border-radius="50%">
+                                    @else
+                                        {{-- <p>{{ Auth::user()->name }}</p> --}}
+                                        <img src="{{ asset('/img/avatar/'.Auth::user()->avatar) }}" alt="avatar" style="width:40px; height:40px; border-radius:50%" border-radius="50%">
+                                    @endif
+                                    {{-- <img class="l-header-dropdown__child-user__img" src="{{ asset('/img/avatar/'.Auth::user()->avatar) }}" alt="user-image" style="width:40px; height:40px; border-radius:50%"> --}}
+                                    <span class="l-header-dropdown__child-user__arrow">â–¼</span>
+                                </div>
+                                <ul>
+                                    {{-- <li><a href="{{ route('mypage.index') }}">{{ __('Mypage') }}</a> --}}
+                                    <li><a href="{{ route('user.profile', Auth::user()->id ) }}">{{ __('Profile') }}</a></li>
+                                    <li><a href="{{ route('user.edit', Auth::user()->id ) }}">{{ __('Edit Profiles') }}</a></li>
+                                    {{-- <li><a href="{{ route('setting.index') }}">{{ __('Setting') }}</a></li> --}}
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <div>
+                            <a class="l-header__signup" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+                        </div>
+                    </li>
+                @endguest
+			</ul>
+		</nav>
+
+</header>
+
+<div id="app">
     <div class="container">
         <div class="card-header">{{ __('Post Register') }}</div>
         <div class="card-body">
@@ -41,17 +112,43 @@
                         {{-- <textarea class=""  cols="50" rows="10"></textarea> --}}
                         {{-- <div id="markdown" name="content">{{ old('content') }} --}}
                             {{-- <mavon-editor v-model="value" language="en" :toolbars="toolbars" /> --}}
-                        {{-- </div> --}}
-                        <textarea id="markdown" name="content"></textarea>
+                            {{-- </div> --}}
+                            <textarea id="editor" name="name" rows="10" cols="50">
+                            </textarea>
 
-                    </div>
 
-                    <div class="">
-                        <button type="submit">{{ __('Register') }}</button>
+                        </div>
+
+                        <div class="">
+                            <button type="submit">{{ __('Register') }}</button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-
-    @endsection
+    <script src="https://cdn.jsdelivr.net/npm/simplemde@latest/dist/simplemde.min.js"></script>
+    <script>
+    // const markdown = new SimpleMDE({
+        //     element: document.getElementById("markdown")
+        // });
+        // Most options demonstrate the non-default behavior
+        const simplemde = new SimpleMDE({
+            element: document.getElementById("editor"),
+            autosave: {
+                enabled: true,
+                delay: 1000,
+            },
+            forceSync: true,
+            parsingConfig: {
+                allowAtxHeaderWithoutSpace: true,
+                strikethrough: true,
+                underscoresBreakWords: true,
+            },
+            placeholder: "Markdown記法で書いてみよう",
+            spellChecker: false,
+            tabSize: 4,
+        });
+    </script>
+</body>
+</html>
