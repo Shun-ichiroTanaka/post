2d1fe780db4a3cd192f38ac9c5cdc96528a8bf8e
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-11-14 23:49:01 +0900

    詳細ページスタイル調整

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,64 +2,68 @@
 @section('content')
 <div id="app">
     <div class="c-post__show">
-        <div class="c-post__show-left">
-            <a href=""><img src="{{ asset('/img/social/twitter.svg') }}" alt="twitter"></a>
-        </div>
-
-        <div class="c-post__show-main">
-            <div class="c-post__show-main__header">
-                <div class="c-post__show-main__header-date">{{$step->created_at}}</div>
-            </div>
-            <div class="c-post__show-title">{{$step->title}}</div>
-
-            <div class="c-post__show-tags">
-                <div class="c-post__show-tags__item">{{$step->tag1}}</div>
-                @if ($step->tag2)
-                <div class="c-post__show-tags__item">{{$step->tag2}}</div>
-                @endif
-                @if ($step->tag3)
-                <div class="c-post__show-tags__item">{{$step->tag3}}</div>
-                @endif
-                @if ($step->tag4)
-                <div class="c-post__show-tags__item">{{$step->tag4}}</div>
-                @endif
-                @if ($step->tag5)
-                <div class="c-post__show-tags__item">{{$step->tag5}}</div>
-                @endif
-            </div>
-
-            <div id="demo">
-                <div class="step-app">
-                    <div class="step-app-side">
-                        <ul class="step-steps">
-                            <li><a href="#step1">{{ $step->subtitle1 }}</a></li>
+        <div id="demo">
+            <div class="c-step">
+                <div class="c-post__show-left">
+                    <p>目標時間 : {{ $step->time }}時間</p>
+                    <div class="step-app-side c-step__side">
+                        <ul class="step-steps c-step__step">
+                            <li><a href="#step1">Step1. {{ $step->subtitle1 }}</a></li>
                             @if ($step->subtitle2)
-                            <li><a href="#step2">{{ $step->subtitle2 }}</a></li>
+                            <li><a href="#step2">2. {{ $step->subtitle2 }}</a></li>
                             @endif
                             @if ($step->subtitle3)
-                            <li><a href="#step3">{{ $step->subtitle3 }}</a></li>
+                            <li><a href="#step3">3. {{ $step->subtitle3 }}</a></li>
                             @endif
                             @if ($step->subtitle4)
-                            <li><a href="#step4">{{ $step->subtitle4 }}</a></li>
+                            <li><a href="#step4">4. {{ $step->subtitle4 }}</a></li>
                             @endif
                         </ul>
                     </div>
+                    <a href=""><img src="{{ asset('/img/social/twitter.svg') }}" alt="twitter"></a>
+                </div>
+
+                <div class="c-post__show-main">
+                    <div class="c-post__show-main__header">
+                        <div class="c-post__show-main__header-date">{{$step->created_at}}</div>
+                    </div>
+                    <div class="c-post__show-title">{{$step->title}}</div>
+
+                    <div class="c-post__show-tags">
+                        <div class="c-post__show-tags__item">{{$step->tag1}}</div>
+                        @if ($step->tag2)
+                        <div class="c-post__show-tags__item">{{$step->tag2}}</div>
+                        @endif
+                        @if ($step->tag3)
+                        <div class="c-post__show-tags__item">{{$step->tag3}}</div>
+                        @endif
+                        @if ($step->tag4)
+                        <div class="c-post__show-tags__item">{{$step->tag4}}</div>
+                        @endif
+                        @if ($step->tag5)
+                        <div class="c-post__show-tags__item">{{$step->tag5}}</div>
+                        @endif
+                    </div>
+
+
+
                     <div class="step-content">
                         <div class="step-tab-panel" id="step1">{!! $step->step1 !!}</div>
                         <div class="step-tab-panel" id="step2">{!! $step->step2 !!}</div>
                         <div class="step-tab-panel" id="step3">{!! $step->step3 !!}</div>
                         <div class="step-tab-panel" id="step4">{!! $step->step4 !!}</div>
                     </div>
-                </div>
-            </div>
 
-        </div>
-        <div class="c-post__show-right">
-            <div class="c-post__show-right__inner">
-                <ul class="step-steps">
-                    <li>目標時間 : {{ $step->time }}時間</li>
-                </ul>
-                <p>このStepにチャレンジ</p>
+
+                </div>
+                <div class="c-post__show-right">
+                    <div class="c-post__show-right__inner">
+                        <ul class="step-steps">
+                            <li>目標時間 : {{ $step->time }}時間</li>
+                        </ul>
+                        <p>このStepにチャレンジ</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
