187c1f50f35269f91fd0e38860c1e355366dfbc2
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-11-29 03:01:32 +0900

    マウスカーソルの追加

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,2 +1,16 @@
-{{-- vueを用いるためこの位置 --}}
+{{-- mouse cursor --}}
+<script>
+    const cursor = document.querySelector('.cursor');
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    })
+    document.addEventListener('click', () => {
+        cursor.classList.add("expand");
+        setTimeout(() => {
+            cursor.classList.remove("expand");
+        }, 500)
+    })
+</script>
+
+// {{-- vueを用いるためこの位置 --}}
 <script src=" {{ mix('js/app.js') }} "></script>
