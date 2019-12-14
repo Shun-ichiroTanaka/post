8c3d79ec17459a8e90aa43199a202d59035299f0
shun tanaka <shun.ichiro.tanaka16@gmail.com>
2019-12-14 13:55:07 +0900

    migrtion ファイル変更

--- database/migrations/2019_12_01_000033_create_tags_table.php
+++ database/migrations/2019_12_01_000033_create_tags_table.php
@@ -15,7 +15,7 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->unique();
+            $table->string('name', 191)->unique();
             $table->timestamps();
         });
     }
