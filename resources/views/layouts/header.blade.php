9516119c5788460fa62158793a9e2b1d7a84939d
shun tanaka <shun.ichiro.tanaka16@gmail.com>
2019-12-15 16:15:56 +0900

    レスポンシブメニュー追加

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,38 +20,64 @@
                             <div href="#" class="l-header-dropdown__child-user">
                                 <div class="">
                                     @if (empty(Auth::user()->avatar))
-                                    <img src="/img/avatar/user.svg" alt="avatar" style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
+                                    <img src="/img/avatar/user.svg" alt="avatar"
+                                        style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
                                     @else
-                                    <img src="{{ asset('/img/avatar/'.Auth::user()->avatar) }}" alt="avatar" style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
+                                    <img src="{{ asset('/img/avatar/'.Auth::user()->avatar) }}" alt="avatar"
+                                        style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
                                     @endif
                                 </div>
                                 <p class="">{{ Auth::user()->name }}</p>
                             </div>
                             <ul>
                                 {{-- <li><a href="{{ route('mypage.index') }}">{{ __('Mypage') }}</a> --}}
-                                    <li><a href="{{ route('user.profile', Auth::user()->id ) }}">{{ __('Mypage') }}</a></li>
-                                    {{-- <li><a href="{{ route('user.edit', Auth::user()->id ) }}">{{ __('Edit Profile') }}</a></li> --}}
-                                    <li>
-                                        <div>
-                                            <a class="" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
+                                <li><a href="{{ route('user.profile', Auth::user()->id ) }}">{{ __('Mypage') }}</a></li>
+                                {{-- <li><a href="{{ route('user.edit', Auth::user()->id ) }}">{{ __('Edit Profile') }}</a>
+                        </li> --}}
+                        <li>
+                            <div>
+                                <a class="" href="{{ route('logout') }}" onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
-                                    </div>
-                                </li>
-                                {{-- <li><a href="{{ route('setting.index') }}">{{ __('Setting') }}</a></li> --}}
-                            </ul>
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">@csrf</form>
+                            </div>
                         </li>
-                    </ul>
-                </li>
-                <li class="c-button-header__post">
-                    <a href="{{ route('post.new') }}">{{ __('Post') }}</a>
-                    {{-- <a href="{{ route('post.index') }}"></a> --}}
-                </li>
-                @endguest
+                        {{-- <li><a href="{{ route('setting.index') }}">{{ __('Setting') }}</a>
+                </li> --}}
+            </ul>
+            </li>
+            </ul>
+            </li>
+            <li class="c-button-header__post">
+                <a href="{{ route('post.new') }}">{{ __('Post') }}</a>
+                {{-- <a href="{{ route('post.index') }}"></a> --}}
+            </li>
+            @endguest
             </ul>
         </nav>
+        <div class="l-header__spnav">
+            <input type="radio" id="one" name="buttons" checked>
+            <label for="one" class="icons home"><a href="/"><span class="fas fa-home"></span></a></label>
+            <input type="radio" id="two" name="buttons">
+            <label for="two" class="icons heart">
+                <a href="{{ route('user.profile', Auth::user()->id ) }}">
+                    @if (empty(Auth::user()->avatar))
+                    <img src="/img/avatar/user.svg" alt="avatar"
+                        style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
+                    @else
+                    <img src="{{ asset('/img/avatar/'.Auth::user()->avatar) }}" alt="avatar"
+                        style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
+                    @endif
+                </a>
+            </label>
+            <input type="radio" id="three" name="buttons">
+            <label for="three" class="icons search"><a href=""><span class="fas fa-search"></span></a></label>
+            <input type="radio" id="four" name="buttons">
+            <label for="four" class="icons bell"><a href="{{ route('post.new') }}"><span class="fas fa-pen-fancy"></span></label>
+            <div id="box">
+            </div>
+        </div>
     </div>
 </header>
