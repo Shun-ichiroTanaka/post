7b306c6d5285b4a5290cb1725f9e29650ee8e1b1
shun tanaka <shun.ichiro.tanaka16@gmail.com>
2019-12-16 19:43:13 +0900

    ユーザー画像の変更

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,7 +20,7 @@
                                     <div class="">
                                         @if (empty(Auth::user()->avatar))
                                         <img src="/img/avatar/user.svg" alt="avatar"
-                                            style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
+                                            style="width:35px; height:35px; background:#fff; padding:1px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
                                         @else
                                         <img src="{{ asset('/img/avatar/'.Auth::user()->avatar) }}" alt="avatar"
                                             style="width:35px; height:35px; border-radius:50%; vertical-align:middle; cursor:pointer; background-size: cover;">
